e213217739b5aa2524d201b9ed780befcadc7880
donaldk12345 <fotsoyvesdonald@gmail.com>
2025-07-04 16:20:19 +0100

    go

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,39 +1,40 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { ApiUrlService } from './api-url.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient,private api:ApiUrlService) {}
 
-  baseUrl = 'http://localhost:8080/api/';
+  //baseUrl = 'http://localhost:8080/api/';
 
   post(url: string, body: any): Observable<any> {
-    return this.httpClient.post(`${this.baseUrl}${url}`,body);
+    return this.httpClient.post(`${this.api.API_URI}${url}`,body);
   }
 
   get(url: string): Observable<any> {
-    return this.httpClient.get(`${this.baseUrl}${url}`);
+    return this.httpClient.get(`${this.api.API_URI}${url}`);
   }
 
     postElementWithParams(url: string, data: any,params:any): Observable<any> {
-    return this.httpClient.post(`${this.baseUrl}${url}`, data,params);
+    return this.httpClient.post(`${this.api.API_URI}${url}`, data,params);
    }
 
       getElementParams(url:string,params:any): Observable<any>{
 
-    return this.httpClient.get(`${this.baseUrl}${url}`,params);
+    return this.httpClient.get(`${this.api.API_URI}${url}`,params);
   }
 
     putElement(url: string, data: any): Observable<any> {
-    return this.httpClient.put(`${this.baseUrl}${url}`, data);
+    return this.httpClient.put(`${this.api.API_URI}${url}`, data);
    }
 
    
   deleteElement(url: string): Observable<any> {
-    return this.httpClient.delete(`${this.baseUrl}${url}`);
+    return this.httpClient.delete(`${this.api.API_URI}${url}`);
   }
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    DEV_BASE_URL: 'http://localhost:8080/api/',
+    PROD_BASE_URL: 'http://ynodev-apps/jwt-cookie/api/'
+};
